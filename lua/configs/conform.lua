a22ddcd85607019b56fc1c50402ec58a0c109f3a
hhheath <heath@edgeimpulse.com>
2024-11-07 15:18:41 -0600

    add prettier & make tab width 4

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,20 +1,24 @@
 local options = {
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "black" },
-	},
+    formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "black" },
+        json = { "prettier" },
+    },
 
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
-	formatters = {
-		-- black is mean so we need to prepend with --fast
-		black = {
-			prepend_args = { "--fast" },
-		},
-	},
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+    formatters = {
+        -- black is mean so we need to prepend with --fast
+        black = {
+            prepend_args = { "--fast" },
+        },
+        prettier = {
+            append_args = { "--tab-width", "4" },
+        },
+    },
 }
 
 return options
